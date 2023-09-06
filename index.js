deac823401528535049a53e5c483770870e0662c
Ryan Davis <rdavis0020@gmail.com>
2023-09-06 15:51:46 -0400

    lab complete

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (parameter1, parameter2)  => parameter1 / parameter2;
+divide(2000,100);
+
+const square = (number) => number * number;
+
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
